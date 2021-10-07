2a75c80aba2e3e3c913a49b89775541d82f3abc9
Alexander Orlovsky <aaorlovsky@hotmail.com>
2021-10-07 15:54:39 +0300

    Some selectors and eventListeners

--- script.js
+++ script.js
@@ -1,18 +1,6 @@
-function doStuff(a, b, name) {
-  print(sum(a, b))
-  print(sayHi(name))
-}
+const link = document.querySelector("[data-link]")
 
-function print(variable) {
-  console.log(variable)
-}
-
-function sum(a, b) {
-  return a + b
-}
-
-function sayHi(name) {
-  return "Hi " + name
-}
-
-doStuff(1, 2, "Alex")
+link.addEventListener("click", (e) => {
+  e.preventDefault()
+  console.log("link is clicked")
+})
