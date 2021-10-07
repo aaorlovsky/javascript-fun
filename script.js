c5898207ee218a9e6a03cd606a34b9bb65eb5b44
Alexander Orlovsky <aaorlovsky@hotmail.com>
2021-10-07 19:47:26 +0300

    First modal app

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
-const link = document.querySelector("[data-link]")
+const modal = document.querySelector("#modal")
+const openModalBtn = document.querySelector("#open-modal-btn")
+const closeModalBtn = document.querySelector("#close-modal-btn")
+const overlay = document.querySelector("#overlay")
 
-link.addEventListener("click", (e) => {
-  e.preventDefault()
-  console.log("link is clicked")
+function removeOpenClass() {
+  modal.classList.remove("open")
+  overlay.classList.remove("open")
+}
+
+openModalBtn.addEventListener("click", () => {
+  modal.classList.add("open")
+  overlay.classList.add("open")
 })
+
+closeModalBtn.addEventListener("click", removeOpenClass)
+
+overlay.addEventListener("click", removeOpenClass)
