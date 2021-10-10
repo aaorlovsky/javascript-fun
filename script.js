8fae8a160e8fe3de95502d8068b1e5349232276f
Alexander Orlovsky <aaorlovsky@hotmail.com>
2021-10-10 12:56:25 +0300

    Adding logic to js error handler

--- script.js
+++ script.js
@@ -1,37 +1,70 @@
-const form = document.querySelector("#quiz-form")
-const answers = document.querySelectorAll(".answer")
-const questions = document.querySelectorAll(".question-item")
-const alertDiv = document.querySelector("#alert")
+// TODO: Select all elements needed
+//    Use the HTML to figure out what classes/ids will work best for selecting each element
+const form = document.getElementById("form")
+const userName = document.getElementById("username")
+const password = document.getElementById("password")
+const passwordConfirmation = document.getElementById("password-confirmation")
+const terms = document.querySelector("#terms")
+
+const errorsList = document.querySelector(".errors-list")
 
 form.addEventListener("submit", (e) => {
-  e.preventDefault()
+  const validationsErrors = []
 
-  answers.forEach((answer) => {
-    answer.closest(".question-item").classList.add("incorrect")
-    answer.closest(".question-item").classList.remove("correct")
-  })
+  if (userName.value.length < 6) {
+    validationsErrors.push("Username has to be at teast 6 characters long")
+  }
 
-  let selectedAnswers = Array.from(answers).filter(
-    (answer) => answer.checked === true
-  )
-  // let countOfCorrectAnswers = 0
-  selectedAnswers.forEach((selectedAnswer) => {
-    if (selectedAnswer.value === "true") {
-      selectedAnswer.closest(".question-item").classList.add("correct")
-      selectedAnswer.closest(".question-item").classList.remove("incorrect")
-      // countOfCorrectAnswers++
-    }
-  })
+  if (password.value.length < 10) {
+    validationsErrors.push("Password has to be at teast 10 characters long")
+  }
 
-  const everythingIsCorrect =
-    selectedAnswers.length === questions.length &&
-    selectedAnswers.every((answer) => answer.value === "true")
+  if (
+    passwordConfirmation.value.length < 10 ||
+    password.value !== passwordConfirmation.value
+  ) {
+    validationsErrors.push("Password confirmation has to be equal password")
+  }
 
-  if (everythingIsCorrect) {
-    document.querySelector("#alert").classList.add("active")
+  if (!terms.checked) {
+    validationsErrors.push("Terms has to be checked")
+  }
 
-    setTimeout(() => {
-      document.querySelector("#alert").classList.remove("active")
-    }, 1000)
+  if (validationsErrors.length > 0) {
+    validationsErrors.forEach((ve) => {
+      const errorLiElement = document.createElement("li")
+      errorLiElement.innerText = ve
+      errorsList.appendChild(errorLiElement)
+    })
+
+    const errors = document.querySelector(".errors")
+    errors.classList.add("show")
+
+    e.preventDefault()
   }
 })
+
+// TODO: Create an event listener for when the form is submitted and do the following inside of it.
+//    TODO: Create an array to store all error messages and clear any old error messages
+//    TODO: Define the following validation checks with appropriate error messages
+//      1. Ensure the username is at least 6 characters long
+//      2. Ensure the password is at least 10 characters long
+//      3. Ensure the password and confirmation password match
+//      4. Ensure the terms checkbox is checked
+//    TODO: If there are any errors then prevent the form from submitting and show the error messages
+
+// TODO: Define this function
+function clearErrors() {
+  // Loop through all the children of the error-list element and remove them
+  // IMPORTANT: This cannot be done with a forEach loop or a normal for loop since as you remove children it will modify the list you are looping over which will not work
+  // I recommend using a while loop to accomplish this task
+  // This is the trickiest part of this exercise so if you get stuck and are unable to progress you can also set the innerHTML property of the error-list to an empty string and that will also clear the children. I recommend trying to accomplish this with a while loop, though, for practice.
+  // Also, make sure you remove the show class to the errors container
+}
+
+// TODO: Define this function
+function showErrors(errorMessages) {
+  // Add each error to the error-list element
+  // Make sure to use an li as the element for each error
+  // Also, make sure you add the show class to the errors container
+}
